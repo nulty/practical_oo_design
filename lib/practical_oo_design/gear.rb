f7dc9ff0482b1886dde9ab4926553298730d1fca
Iain McNulty <iainmcnulty@gmail.com>
2014-05-16 18:58:30 +0100

    Added Wheel struct to gear class (p32)

--- lib/practical_oo_design/gear.rb
+++ lib/practical_oo_design/gear.rb
@@ -1,10 +1,9 @@
 class Gear
-  attr_reader :chainring, :cog, :rim, :tire
+  attr_reader :chainring, :cog, :wheel
   def initialize(chainring, cog, rim, tire)
     @chainring = chainring
     @cog       = cog
-    @rim       = rim
-    @tire      = tire
+    @wheel     = Wheel.new(rim, tire)
   end
 
   def ratio
@@ -15,10 +14,9 @@ class Gear
     ratio * diameter
   end
 
-  def diameter
-    rim + (tire * 2)
+  Wheel = Struct.new(:rim, :tire) do
+    def diameter
+      rim + (tire * 2)
+    end
   end
 end
-
-# puts Gear.new(52, 11, 26, 1.5).gear_inches
-# puts Gear.new(52, 11, 24, 1.25).gear_inches
