ee6a0bd841707b1159cb1260a7016b169fc89f2b
Iain McNulty <iainmcnulty@gmail.com>
2014-05-16 21:15:27 +0100

    Add Trip case statement (p88)

--- lib/practical_oo_design/trip.rb
+++ lib/practical_oo_design/trip.rb
@@ -2,7 +2,33 @@ class Trip
 
   attr_reader :bicycles, :customers, :vehicle
 
-  def prepare(mechanic)
-    mechanic.prepare_bicycles(bicycles)
+  def prepare(preparers)
+    preparers.each { |preparers|
+      case preparers
+      when Mechanic
+        preparer.prepare_bicycles(bicycle)
+      when TripCoordinator
+        preparer.buy_food(customers)
+      when Driver
+        preparer.gas_up(vehicle)
+        preparer.fill_water_tank(vehicle)
+      end
+    }
+  end
+end
+
+class TripCoordinator
+  def buy_food(customers)
+    # ...
+  end
+end
+
+class Driver
+  def gas_up(vehicle)
+    # ...
+  end
+
+  def fill_water_tank(vehicle)
+    # ...
   end
 end
