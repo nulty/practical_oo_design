4f475091c01574466778c1115e6e6ce9949ea65d
Iain McNulty <iainmcnulty@gmail.com>
2014-05-16 22:10:16 +0100

    Add inherited RoadBike < Bicycle, move all funcionality form Bicycle to RioadBike (p119-120)

--- lib/practical_oo_design.rb
+++ lib/practical_oo_design.rb
@@ -3,8 +3,21 @@ require_relative "practical_oo_design/gear"
 require_relative "practical_oo_design/wheel"
 require_relative "practical_oo_design/bicycle"
 require_relative "practical_oo_design/mountain_bike"
+require_relative "practical_oo_design/road_bike"
 
 module PracticalOoDesign
+
+  method = <<-BIKE.chomp
+road_bike = RoadBike.new(
+                  size: 'M',
+                  tape_color: 'red' )
+# -> 
+BIKE
+  print method
+  puts road_bike = RoadBike.new(size: 'M', tape_color: 'red')
+
+  print "road_bike.size\n# -> "
+  puts road_bike.size
 
   method = <<-BIKE.chomp
 mountain_bike = MountainBike.new(
@@ -16,9 +29,6 @@ BIKE
   print method
   puts mountain_bike = MountainBike.new(size: 'S', front_shock: 'Manitou', rear_shock: 'Fox')
 
-  print "bike.size\n# -> "
+  print "mountain_bike.size\n# -> "
   puts mountain_bike.size
-
-  print "bike.spares\n# -> "
-  puts mountain_bike.spares
 end
--- lib/practical_oo_design/bicycle.rb
+++ lib/practical_oo_design/bicycle.rb
@@ -1,20 +1,4 @@
 class Bicycle
-  attr_reader :style, :size, :tape_color,
-              :front_shock, :rear_shock
-
   def initialize(args)
-    @type        = args[:style]
-    @size        = args[:size]
-    @tape_color  = args[:tape_color]
-    @front_shock = args[:front_shock]
-    @rear_shock  = args[:rear_shock]
-  end
-
-  def spares
-    { chain:       '10-speed',
-      tire_size:   '23',
-      tape_color:  tape_color,
-      front_shock: front_shock,
-      rear_shock:  rear_shock }
   end
 end
