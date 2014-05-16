c95a2911d007c8225191eac7e94bc199fbdcb57a
Iain McNulty <iainmcnulty@gmail.com>
2014-05-16 19:11:38 +0100

    Added Wheel class and inject Wheel into gear class (p34)

--- lib/practical_oo_design.rb
+++ lib/practical_oo_design.rb
@@ -1,5 +1,16 @@
-require "practical_oo_design/version"
+require_relative "practical_oo_design/version"
+require_relative "practical_oo_design/gear"
+require_relative "practical_oo_design/wheel"
 
 module PracticalOoDesign
-      # Your code goes here...
+  @wheel = Wheel.new(26, 1.5)
+  puts "@wheel = Wheel.new(26, 1.5)\n"
+  print "@wheel.circumference\n# -> "
+  puts @wheel.circumference
+
+  print "Gear.new(52, 11, @wheel).gear_inches\n# -> "
+  puts Gear.new(52, 11, @wheel).gear_inches
+
+  print "Gear.new(52, 11).ratio\n# -> "
+  puts Gear.new(52, 11).ratio
 end
--- lib/practical_oo_design/gear.rb
+++ lib/practical_oo_design/gear.rb
@@ -1,9 +1,9 @@
 class Gear
   attr_reader :chainring, :cog, :wheel
-  def initialize(chainring, cog, rim, tire)
+  def initialize(chainring, cog, wheel=nil)
     @chainring = chainring
     @cog       = cog
-    @wheel     = Wheel.new(rim, tire)
+    @wheel     = wheel
   end
 
   def ratio
@@ -11,12 +11,6 @@ class Gear
   end
 
   def gear_inches
-    ratio * diameter
-  end
-
-  Wheel = Struct.new(:rim, :tire) do
-    def diameter
-      rim + (tire * 2)
-    end
+    ratio * wheel.diameter
   end
 end
