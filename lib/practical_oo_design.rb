72cc4d8cee6e065005aafa674ceb77da449bcfe8
Iain McNulty <iainmcnulty@gmail.com>
2014-05-17 01:41:42 +0100

    Add Vehicle and make Vehicle and Mechanic Schedulable

--- lib/practical_oo_design.rb
+++ lib/practical_oo_design.rb
@@ -9,52 +9,20 @@ require_relative "practical_oo_design/road_bike"
 require_relative "practical_oo_design/recumbent_bike"
 require_relative "practical_oo_design/schedulable"
 require_relative "practical_oo_design/schedule"
+require_relative "practical_oo_design/vehicle"
+require_relative "practical_oo_design/mechanic"
 
 module PracticalOoDesign
-
-#   method = <<-BIKE.chomp
-# road_bike = RoadBike.new(
-#                   size: 'M',
-#                   tape_color: 'red' )
-# # -> 
-# BIKE
-#   print method
-#   puts road_bike = RoadBike.new(size: 'M', tape_color: 'red')
-
-  # print "road_bike.size\n# -> "
-  # puts road_bike.size
-
-  # print "road_bike.tire_size\n# -> "
-  # puts road_bike.tire_size
-
-  # print "road_bike.chain\n# -> "
-  # puts road_bike.chain
-
-#   method = <<-BIKE.chomp
-# mountain_bike = MountainBike.new(
-#                   size: 'S',
-#                   front_shock: 'Manitou',
-#                   rear_shock: 'Fox' )
-# # -> 
-# BIKE
-#   print method
-#   puts mountain_bike = MountainBike.new(size: 'S', front_shock: 'Manitou', rear_shock: 'Fox')
-
-  # print "mountain_bike.size\n# -> "
-  # puts mountain_bike.size
-
-  # print "mountain_bike.tire_size\n# -> "
-  # puts mountain_bike.tire_size
-
-  # print "mountain_bike.chain\n# -> "
-  # puts mountain_bike.chain
-
-  # print "recumbent_bike = RecumbentBike.new\n# -> "
-  # puts recumbent_bike = RecumbentBike.new(flag: "tall and orange")
 
   starting = Date.parse("2015/09/04")
   ending   = Date.parse("2015/09/10")
 
-  b = Bicycle.new
-  b.schedulable?(starting,ending)
+  v = Vehicle.new
+  v.schedulable?(starting,ending)
+
+  starting = Date.parse("2015/09/04")
+  ending   = Date.parse("2015/09/10")
+
+  m = Mechanic.new
+  m.schedulable?(starting,ending)
 end
--- lib/practical_oo_design/mechanic.rb
+++ lib/practical_oo_design/mechanic.rb
@@ -1,4 +1,10 @@
 class Mechanic
+  include Schedulable
+
+  def lead_days
+    4
+  end
+
   def prepare_trip(trip)
     trip.bicycles.each { |bicycle|
       prepare_bicycle(bicycle) }
