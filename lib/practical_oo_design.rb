4911bde9ec97273457a3851aa9ce72148889d627
Iain McNulty <iainmcnulty@gmail.com>
2014-05-17 02:03:36 +0100

    Add Parts hierarchy and update *Bicycle classes (p165-168)

--- lib/practical_oo_design.rb
+++ lib/practical_oo_design.rb
@@ -11,18 +11,53 @@ require_relative "practical_oo_design/schedulable"
 require_relative "practical_oo_design/schedule"
 require_relative "practical_oo_design/vehicle"
 require_relative "practical_oo_design/mechanic"
+require_relative "practical_oo_design/parts"
+require_relative "practical_oo_design/mechanic"
+require_relative "practical_oo_design/road_bike_parts"
+require_relative "practical_oo_design/mountain_bike_parts"
 
 module PracticalOoDesign
 
-  starting = Date.parse("2015/09/04")
-  ending   = Date.parse("2015/09/10")
+  method = <<-BIKE.chomp
+road_bike =
+  Bicycle.new(
+    size: 'L',
+    parts: RoadBikeParts.new(tape_color: 'red'))\n# -> 
+  BIKE
 
-  v = Vehicle.new
-  v.schedulable?(starting,ending)
+  puts
+  print method
+  puts road_bike = Bicycle.new(size: 'L', parts: RoadBikeParts.new(tape_color: 'red'))
+  puts
 
-  starting = Date.parse("2015/09/04")
-  ending   = Date.parse("2015/09/10")
+  print "road_bike.size\n# -> "
+  puts road_bike.size
+  puts
 
-  m = Mechanic.new
-  m.schedulable?(starting,ending)
+  print "road_bike.spares\n# -> "
+  puts road_bike.spares
+  puts
+
+
+
+  method = <<-BIKE.chomp
+mountain_bike =
+  Bicycle.new(
+    size: 'L',
+    parts: MountainBikeParts.new(rear_shock: 'Fox'))\n# -> 
+  BIKE
+
+  print method
+  puts mountain_bike = Bicycle.new(size: 'L', parts: MountainBikeParts.new(rear_shock: 'Fox'))
+  puts
+
+  print "mountain_bike.size\n# -> "
+  puts mountain_bike.size
+  puts
+
+  print "mountain_bike.spares\n# -> "
+  puts mountain_bike.spares
+  puts
+
+
 end
--- lib/practical_oo_design/bicycle.rb
+++ lib/practical_oo_design/bicycle.rb
@@ -1,43 +1,12 @@
-require_relative "schedulable"
 class Bicycle
-  include Schedulable
-  attr_reader :schedule, :size, :chain, :tire_size
+  attr_reader :size, :parts
 
   def initialize(args={})
-    @schedule  = args[:schedule]  || ::Schedule.new
     @size      = args[:size]
-    @chain     = args[:chain]     || default_chain
-    @tire_size = args[:tire_size] #|| default_tire_size
-
-    post_initialize(args)   # Bicycle both sends
-  end
-
-  # def scheduled?(start_date, end_date)
-  #   schedule.scheduled?(self, start_date, end_date)
-  # end
-
-  def lead_days
-    1
-  end
-
-  def default_chain
-    '10-speed'
-  end
-
-  def default_tire_size
-    raise NotImplementedError, "is not implemented in #{self.class}"
+    @parts     = args[:parts]
   end
 
   def spares
-    { tire_size: tire_size,
-      chain:     chain}.merge(local_spares)
-  end
-
-  def post_initialize(args)  # and implements this
-    nil
-  end
-
-  def local_spares
-    {}
+    parts.spares
   end
 end
