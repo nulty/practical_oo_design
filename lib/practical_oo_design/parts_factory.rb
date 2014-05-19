3ed983df75c8e93cb6b867ce7587f8a96a437c4a
Iain McNulty <iainmcnulty@gmail.com>
2014-05-19 19:41:44 +0100

    Move Parts class into PartsFactory to avoid duplication (p179-180)

--- lib/practical_oo_design/parts_factory.rb
+++ lib/practical_oo_design/parts_factory.rb
@@ -1,14 +1,16 @@
+require 'ostruct'
 module PartsFactory
 
-  def self.build(config,
-                 part_class:  Part,
-                 parts_class: Parts)
+  def self.build(config, parts_class = Parts)
+    parts_class.new(
+      config.collect {|part_config|
+        create_part(part_config) } )
+  end
 
-  parts_class.new(
-    config.collect {|part_config|
-      part_class.new(
-        name:         part_config[0],
-        description:  part_config[1],
-        needs_spares: part_config.fetch(2, true) ) } )
+  def self.create_part(part_config)
+    OpenStruct.new(
+      name:        part_config[0],
+      description: part_config[1],
+      needs_spare: part_config.fetch(2, true))
   end
 end
